fb0bd77dc81323a36fef3ba0596a709225911a20
ParkYoungWoong <thesecon@gmail.com>
2026-07-10 11:45:07 +0900

    26-07-10 11:45

--- src/components/dashboards/Nav.tsx
+++ src/components/dashboards/Nav.tsx
@@ -10,17 +10,23 @@ const navigations = [
 export default function Nav() {
   return (
     <nav className="fixed top-[32px] left-0 box-border h-[calc(100vh-32px)] w-[250px] border-r border-gray-200 bg-white p-4">
-      <NavLink
-        to="/dashboard"
-        className={({ isActive }) => {
-          const base =
-            'mb-1 flex h-[40px] w-full items-center rounded-md px-3 hover:bg-gray-200'
-          return isActive
-            ? twMerge(base, 'bg-red-200 font-bold hover:bg-red-300')
-            : base
-        }}>
-        대시보드
-      </NavLink>
+      {navigations.map(nav => {
+        return (
+          <NavLink
+            key={nav.to}
+            to={nav.to}
+            end
+            className={({ isActive }) => {
+              const base =
+                'mb-1 flex h-[40px] w-full items-center rounded-md px-3 hover:bg-gray-200'
+              return isActive
+                ? twMerge(base, 'bg-red-200 font-bold hover:bg-red-300')
+                : base
+            }}>
+            {nav.label}
+          </NavLink>
+        )
+      })}
     </nav>
   )
 }
--- src/routes/pages/dashboards/Index.tsx
+++ src/routes/pages/dashboards/Index.tsx
@@ -1,4 +1,38 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+export interface Dashboard {
+  totalStores: number
+  activeStores: number
+  totalSales: number
+  avgSales: number
+  regionSales: RegionSale[]
+  monthlySales: MonthlySale[]
+  pieData: PieDaum[]
+}
+export interface RegionSale {
+  region: string
+  total: number
+}
+export interface MonthlySale {
+  month: string
+  total: number
+}
+export interface PieDaum {
+  name: string
+  value: number
+}
+
 export default function Index() {
+  const [dashboard, setDashboard] = useState<Dashboard | null>(null)
+
+  useEffect(() => {
+    ;(async function () {
+      const { data } = await axios.get('http://localhost:3000/dashboard')
+      setDashboard(data)
+    })()
+  }, [])
+
   return (
     <>
       <h1>대시보드</h1>
